150f722675471e1ae9c6d97cddc43a1b4f6b9726
RogerFK <jorgg69@gmail.com>
2020-10-25 23:45:03 +0100

    Added project files

---
+++ Labrunning/LabRunPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+
+using HarmonyLib;
+
+using Labrunning.Patches;
+
+namespace Labrunning
+{
+	public class LabRunPlugin : Plugin<LabRunPlugin.Configs>
+	{
+		public class Configs : IConfig
+		{
+			public bool IsEnabled { get; set; } = true;
+		
+		}
+		private int staticIncrementer = -1;
+		public Harmony HarmonyInstance { get; set; } = null;
+
+		
+		private MethodInfo svsideUpdate = null;
+
+		public override void OnEnabled()
+		{
+			HarmonyInstance = new Harmony($"LabRunning-{staticIncrementer++}");
+			try
+			{
+
+				// Get the ServesideUpdate method. Hopefully it's not Ambiguous
+				svsideUpdate = typeof(Stamina).GetMethod("ServesideUpdate",
+						BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+				if (svsideUpdate == null)
+				{
+					// They fixed the name if it's null, get the method again lol
+					svsideUpdate = typeof(Stamina).GetMethod("ServersideUpdate",
+						BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+
+					if (svsideUpdate == null)
+					{
+						Log.Error("Can't find ServersideUpdate in class Stamina. This plugin is outdated, feel free to delete it.");
+						return;
+					}
+				}
+
+				// Patch the method
+				HarmonyInstance.Patch(svsideUpdate, null, null, new HarmonyMethod(typeof(ProcessStaminaOverride), nameof(ProcessStaminaOverride.Transpiler)));
+				base.OnEnabled();
+
+			} catch (AmbiguousMatchException)
+			{
+				Log.Error("AmbiguousMatchException! Labrunning will not work until it gets updated, feel free to delete the plugin.");
+			} catch (Exception ex)
+			{
+				Log.Error(ex.ToString());
+				Log.Info("Don't worry: if you're seeing this message, your server is fine but the plugin is outdated. Feel free to delete the plugin.");
+			}
+		}
+
+		public override void OnDisabled()
+		{
+			if (HarmonyInstance != null && svsideUpdate != null)
+			{
+				HarmonyInstance.Unpatch(svsideUpdate, HarmonyPatchType.Transpiler, HarmonyInstance.Id);
+				base.OnDisabled();
+			}
+		}
+	}
+
+}
---
+++ Labrunning/Patches/ProcessStaminaOverride.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Exiled.API.Features;
+
+using HarmonyLib;
+
+namespace Labrunning.Patches
+{
+	class ProcessStaminaOverride
+	{
+		// Get the method info. Scan for Public and NonPublic, since they might change it.
+		private static readonly MethodInfo _processStamina = typeof(Stamina).GetMethod("ProcessStamina",
+																	   BindingFlags.Public |
+																	   BindingFlags.NonPublic |
+																	   BindingFlags.Instance |
+																	   BindingFlags.IgnoreCase);
+		private static readonly MethodInfo _customStamina = typeof(ProcessStaminaOverride).GetMethod("CustomProcessStamina",
+																					BindingFlags.Public |
+																					BindingFlags.NonPublic |
+																					BindingFlags.Static |
+																					BindingFlags.IgnoreCase);
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var found = false;
+			if (_processStamina == null)
+			{
+				Log.Error("Can't find ProcessStamina in the Stamina class. Don't worry, your server is fine, Labrunning is just outdated.");
+				foreach (var instruction in instructions) yield return instruction;
+				yield break;
+			}
+
+			foreach (var instruction in instructions)
+			{
+
+				if (instruction.Calls(_processStamina))
+				{
+					// These two yields do the following:
+					// 1. Load the current instance into the stack
+					// 2. Call the CustomProcessStamina function
+					//
+					// In a nutshell, it just calls CustomProcessStamina 
+					// with the instance as a parameter.
+
+					yield return new CodeInstruction(OpCodes.Ldarg_0);
+					yield return new CodeInstruction(OpCodes.Call, _customStamina);
+					found = true;
+				}
+
+				yield return instruction;
+
+			}
+			if (!found) Log.Error($"Can't find instruction ProcessStamina in method ServersideUpdate. Labrunning is outdated, feel free to remove it.");
+		}
+
+		public static void CustomProcessStamina(Stamina _this)
+		{
+
+		}
+	}
+}
